38caea3d89633415f81f29146099ef7ba3937efd
felixmellitzer <felix@tfm.agency>
2020-10-20 14:44:28 +0200

    setup project in one plugin file

---
+++ periodic-nginx-cache-purger.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Periodic Nginx Cache Purger
+ *
+ * @package           PluginPackage
+ * @author            Felix Mellitzer
+ * @copyright         2020 TFM Agency GmbH
+ * @license           GPL-2.0-or-later
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Periodic Nginx Cache Purger
+ * Description:       purges cache periodically with Nginx Helper WordPress Plugin
+ * Version:           1.0.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.2
+ * Author:            Felix Mellitzer
+ * Author URI:        https://tfm.agency
+ * Text Domain:       periodic-nginx-cache-purger
+ * License:           GPL v2 or later
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ */
